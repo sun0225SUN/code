751534240855dc928f5941ee5c7e4ef3b54b3216
sunshine <sun412926@gmail.com>
2021-09-22 10:37:31 +0800

    小改动

--- python作业/2019306264_孙国旗_作业2.py
+++ python作业/2019306264_孙国旗_作业2.py
@@ -1,12 +1,12 @@
 # 新建一个列表，存储学生信息，学生的信息采用字典的方式存储，0表示女性，1表示男性
-info = [{'name': '小千', 'sex': '0', 'score': '95'},
-        {'name': '小锋', 'sex': '1', 'score': '99'},
-        {'name': '小扣', 'sex': '0', 'score': '86'},
-        {'name': '小丁', 'sex': '1', 'score': '88'},
-        {'name': '小明', 'sex': '1', 'score': '90'}]
+info = [{'name': '小千', 'sex': 0, 'score': 95},
+        {'name': '小锋', 'sex': 1, 'score': 99},
+        {'name': '小扣', 'sex': 0, 'score': 86},
+        {'name': '小丁', 'sex': 1, 'score': 88},
+        {'name': '小明', 'sex': 1, 'score': 90}]
 # filter() 函数用于过滤序列，过滤掉不符合条件的元素，（返回由符合条件元素组成的新列表）python3中返回的是一个fileter对象，所以需要转换成列表。
 # 语法为  filter(function, iterable) 参数分别为一个函数和一个可迭代的对象，本处表示过滤掉女性童鞋。
-temp1 = list(filter(lambda x: x['sex'] == '1', info))
+temp1 = list(filter(lambda x: x['sex'] == 1, info))
 # sorted函数是python中的内置函数，用于对所有可迭代对象进行排序操作。
 # 语法为sorted(iterable, cmp=None, key=None, reverse=False)。
 # 第一个参数是一个可迭代对象，cmp和key为比较函数，cmp可以拥有两个参数，key只允许一个参数，默认由低到高排序，reverse设置为ture为逆序。
@@ -25,25 +25,22 @@ for x in temp2:
     # 继续循环，遍历下一名学生⬆️
 
 
-# 吐槽一下，PPT里的程序是有问题的，虽然问题很小，但是debug还是浪费了一点时间。
-# 问题就在于过滤列表中的过滤函数判断项，应该是x['sex']=='1',而不是x['sex']==1，要不然所有的数据都过滤掉了。
-
 
 # 将lambda函数改成标准函数。
 
 # def del_girl(x):
-#     return x['sex'] == '1'
+#     return x['sex'] == 1
 #
 #
 # def sort_score(x):
 #     return x['score']
 #
 #
-# info = [{'name': '小千', 'sex': '0', 'score': '95'},
-#         {'name': '小锋', 'sex': '1', 'score': '99'},
-#         {'name': '小扣', 'sex': '0', 'score': '86'},
-#         {'name': '小丁', 'sex': '1', 'score': '88'},
-#         {'name': '小明', 'sex': '1', 'score': '90'}]
+# info = [{'name': '小千', 'sex': 0, 'score': 95},
+#        {'name': '小锋', 'sex': 1, 'score': 99},
+#        {'name': '小扣', 'sex': 0, 'score': 86},
+#        {'name': '小丁', 'sex': 1, 'score': 88},
+#        {'name': '小明', 'sex': 1, 'score': 90}]
 # temp1 = list(filter(del_girl, info))
 # temp2 = sorted(temp1, key=sort_score, reverse=True)
 # for x in temp2:
@@ -51,3 +48,4 @@ for x in temp2:
 #         if key != 'sex':
 #             print(value, end=' ')
 #     print()
+
