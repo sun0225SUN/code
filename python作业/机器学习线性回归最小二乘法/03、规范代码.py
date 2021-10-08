587a099239e96555ce54625ac6dcaed5341594e4
sunshine <sun412926@gmail.com>
2021-10-08 16:51:16 +0800

    Update 03、规范代码.py

--- python作业/机器学习线性回归最小二乘法/03、规范代码.py
+++ python作业/机器学习线性回归最小二乘法/03、规范代码.py
@@ -6,15 +6,14 @@ import matplotlib.pyplot as plt
 class Solution:
     # 定义损失函数
     def compute_cost(self, w, b, points):
-        total_cost = 0  # 损失误差
-        M = len(points)  # 点的个数
-        # 逐点计算平方损失误差，然后求平均值
+        total_cost = 0
+        M = len(points)
         for i in range(M):
-            x = points[i, 0]  # 表示第一列第i个点，即xi
-            y = points[i, 1]  # 表示第二列低i个点，即yi
-            total_cost += (y - w * x - b) ** 2  # 这里y表示实际值，wx+b表示估计值，单个点误差的平方为(y-(wx+b))²，+=计算累加值
+            x = points[i, 0]
+            y = points[i, 1]
+            total_cost += (y - w * x - b) ** 2
 
-        return total_cost / M  # 函数返回误差的平方和的平均数（本例的损失函数可以理解为计算方差）
+        return total_cost / M 
 
     # 定义了一个求取平均数的函数，因为要计算x_bar，y_bar
     def average(self, data):
@@ -26,17 +25,17 @@ class Solution:
 
     # 拟合函数
     def fit(self, points):
-        M = len(points)  # 点的个数
-        x_bar = self.average(points[:, 0])  # 第一列所有点的平均值
-        y_bar = self.average(points[:, 1])  # 第二列所有点的平均值
-        sum_yx = 0  # 计算分子累加和部分
-        sum_x2 = 0  # 计算分母累加和部分,累加要有初始值，初始化为0
+        M = len(points)
+        x_bar = self.average(points[:, 0])
+        y_bar = self.average(points[:, 1])
+        sum_yx = 0
+        sum_x2 = 0
 
         for i in range(M):
-            x = points[i, 0]  # 第一列第i个点，即xi
-            y = points[i, 1]  # 第二列第i个点，即yi
-            sum_yx += y * x  # 分子累加部分
-            sum_x2 += x ** 2  # 分母累加部分
+            x = points[i, 0]
+            y = points[i, 1]
+            sum_yx += y * x
+            sum_x2 += x ** 2
 
         # 根据公式计算W
         fz = sum_yx - M * x_bar * y_bar
@@ -72,8 +71,12 @@ if __name__ == '__main__':
     # 打印
     print("cost is:", cost)
 
+    # 展示拟合曲线
+    # 画出散点图
+    plt.scatter(x, y)
     # 针对每一个x,计算出预测的y值
     pred_y = w * x + b
-    # 画出拟合曲线
-    plt.scatter(x, pred_y)
+    # 画出拟合线
+    plt.plot(x, pred_y, c='r')
+    # 展示
     plt.show()
